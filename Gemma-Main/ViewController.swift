98e3a41a1a1c50f9d5cabdb2a134c6e853ae49e9
koofrank <koofranker@gmail.com>
2018-10-10 12:21:01 +0800

    init project

---
+++ Gemma-Main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gemma-Main
+//
+//  Created by koofrank on 2018/10/10.
+//  Copyright © 2018 com.nbltrustdev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Gemma-MainTests/Gemma_MainTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gemma_MainTests.swift
+//  Gemma-MainTests
+//
+//  Created by koofrank on 2018/10/10.
+//  Copyright © 2018 com.nbltrustdev. All rights reserved.
+//
+
+import XCTest
+@testable import Gemma_Main
+
+class Gemma_MainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
